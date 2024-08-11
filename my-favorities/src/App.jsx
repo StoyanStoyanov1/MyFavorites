ac3492a0969953681065e51086ca6b71562fd395
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-11 16:13:32 +0200

    config the vite project

---
+++ my-favorities/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Navigate} from "react-router-dom";
+import Navigation from "./components/navigation/Navigation.jsx";
+
+function  App() {
+	return (
+		<div id='box'>
+		<Navigation/>
+		</div>
+	)
+}
+
+export default App;
