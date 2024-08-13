0fc7b32e194b1d54ad2d7d2640fa96526a0b51dc
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-13 08:40:22 +0200

    config server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const port = 3030;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cookieParser());
 app.use(express.json());
 
 mongoose.connection.on('connected', () => console.log('MongoDB Connected'));
 mongoose.connection.on('disconnected', () => console.log('MongoDB Disconnected'));
 mongoose.connection.on('error', err => console.log('MongoDB Error:', err));
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
