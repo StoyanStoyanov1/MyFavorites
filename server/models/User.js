7d2d6f6367f4e78b13fe7c9f6f8bae371684a6fb
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-15 10:19:50 +0200

    add e model User

---
+++ server/models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	gender: {
+		type: String,
+		required: true,
+	},
+	birthDate: {
+		type: String,
+		required: true,
+	},
+	books: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Book"
+	}],
+	podcasts: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'PodCast'
+	}],
+	movies: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Movie'
+	}],
+	messages: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Message'
+	}
+	]
+
+}, {timestamps: true});
+
+userSchema.pre('save', async function (next) {
+	if (this.isModified('password')) {
+		this.password = await bcrypt.hash(this.password, 12);
+	}
+
+	next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
