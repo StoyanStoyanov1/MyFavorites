cdc35a06f521a66753f1d1d8951daa14815169be
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-16 11:46:25 +0200

    config request

---
+++ my-favorites/src/lib/request.js
@@ -0,0 +1,41 @@
+import {options} from "axios";
+
+const buildOptions = (data) => {
+	const options = {};
+
+	if(data) {
+		options.body = JSON.stringify(data);
+		options.headers = {'Content-Type': 'application/json'};
+	}
+
+	const token = localStorage.getItem('accessToken');
+
+	if (token) {
+		options.headers = {
+			...options.headers,
+			'X-Authorization': token
+		};
+	}
+
+	return options;
+}
+
+export default async function request(method, url, data)  {
+	const response = await fetch(url, {
+		method,
+		credentials: 'include',
+		...buildOptions(data)
+	});
+
+	if (response.status === 204) {
+		return {};
+	}
+
+	const result = await response.json();
+
+	if (!response.ok) {
+		throw result;
+	}
+
+	return result;
+}
