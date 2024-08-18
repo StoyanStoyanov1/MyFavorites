b355b9fa88b7ed7d932341decf3bb006f92d0138
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-19 00:07:16 +0200

    model Content

---
+++ server/models/Content.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const contentSchema = mongoose.Schema({
+	type: {
+		type: String,
+		enum: ['book', 'movie', 'podcast'],
+		required: true
+	},
+	title: {
+		type: String,
+		require: true,
+		trim: true,
+		validate: {
+			validator: (value) => {
+				return value[0] !== ' ' && value.length >= 2
+			},
+			message: "The title cannot start with a blank space and must be at least 2 characters long.",
+
+		}
+	},
+	genre: {
+		type: String,
+		require: true,
+	},
+	year: {
+		type: String,
+		require: true,
+	},
+	description: {
+		type: String,
+		require: true,
+		trim: true,
+		validate: {
+			validator: (value) => {
+				return value.length >= 20;
+			},
+			message: "The description must be at least 20 characters long."
+		},
+	}
+
+
+
+}, {timestamps: true});
+
+const Content = mongoose.model('Book', contentSchema)
+
+module.exports = Content;
