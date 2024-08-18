c040b9aef7cdc806ee61518a7bc468b945bc59c3
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-18 11:06:35 +0200

    validate User model

--- server/models/User.js
+++ server/models/User.js
@@ -5,14 +5,42 @@ const userSchema = mongoose.Schema({
 	username: {
 		type: String,
 		required: true,
+		validate: {
+			validator: (value) => {
+				return !(value.length > 2) || value.length > 12;
+			},
+			message: "The username must be between 2 and 12 characters long."
+		}
 	},
 	email: {
 		type: String,
 		required: true,
+		validate: {
+			validator: (value) => {
+				return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+			},
+			message: (props) => `${props.value} is not a valid email!`
+
+		}
 	},
 	password: {
 		type: String,
 		required: true,
+		validate: {
+			validator: (value) => {
+				if (value.length < 6) {
+					return false;
+				}
+				if (!/[a-z]/.test(value)) {
+					return false;
+				}
+				if (!/[A-Z]/.test(value)) {
+					return false;
+				}
+				return /[0-9]/.test(value);
+			},
+			message: "The password needs one lowercase, one uppercase letter, one digit, and at least 6 characters."
+		}
 	},
 	gender: {
 		type: String,
