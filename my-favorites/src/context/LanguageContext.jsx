a4bbf11e2f4490b2c5ac5bded8659a8fed3657af
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-18 19:06:58 +0200

    install and config js-cookie

--- my-favorites/src/context/LanguageContext.jsx
+++ my-favorites/src/context/LanguageContext.jsx
@@ -1,17 +1,26 @@
-import React, {createContext, useState, useContext} from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 const LanguageContext = createContext();
 
 export function LanguageProvider({ children }) {
-	const [language, setLanguage] = useState('en');
+
+	const [language, setLanguage] = useState(() => {
+		return Cookies.get('language') || 'en';
+	});
+
+	const updateLanguage = (newLanguage) => {
+		setLanguage(newLanguage);
+		Cookies.set('language', newLanguage, { expires: 365 });
+	};
 
 	return (
-		<LanguageContext.Provider value={[language, setLanguage]}>
+		<LanguageContext.Provider value={[language, updateLanguage]}>
 			{children}
 		</LanguageContext.Provider>
 	)
 }
 
 export function useLanguage() {
-	return useContext(LanguageContext)
+	return useContext(LanguageContext);
 }
