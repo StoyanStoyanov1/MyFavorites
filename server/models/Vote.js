eaa78d20dcc6299c57359d09ed75d0413b40346e
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-27 08:52:32 +0200

    update Vote model

--- server/models/Vote.js
+++ server/models/Vote.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose');
 
 const voteSchema = mongoose.Schema({
-	userId: {
-		type: mongoose.Schema.Types.ObjectId,
-		required: true,
-		ref: 'User'
-	},
-	rating: {
-		type: Number,
-		required: true,
+	reviewers: {
+		type: Map,
+		of: Number,
+		required: true
 	}
-}, {timestamps: true});
+}, { timestamps: true });
 
 const Vote = mongoose.model('Vote', voteSchema);
 
 module.exports = Vote;
-
