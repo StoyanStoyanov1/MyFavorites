57508a9cbec7a14bf815478f51d3af407e310b5f
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-09-03 12:41:48 +0200

    Create a model Comment

---
+++ server/models/Comment.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const commentSchema = mongoose.Schema({
+	contentId: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+	},
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+	},
+	text: {
+		type: String,
+		required: true,
+		validate: {
+			validator: (v) => v.length > 0,
+		},
+		message: "The message cannot be empty.",
+	}
+}, {timestamps: true});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
